eec8198fd591c74f6aed6fbd06422c2bb46df884
gustaacamargo <camargo25.gustavo@gmail.com>
2020-05-24 19:58:30 -0300

    corrige estilização

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -5,6 +5,7 @@ export const Header = styled.div`
     background-color: ${props => props.theme.colors.primary};
     display: flex;
     align-items: center;
+    justify-content: space-between;
     color: #FFF;
     padding: 0 30px;
 `;
